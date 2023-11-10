f69344a202fa4852e2acbfc69bf36afa2f98b0d3
ignac <ignac@Ignacio.fibertel.com.ar>
2023-11-09 21:30:31 -0300

    Nueva version

--- TrabajoFinalTaller/src/main/java/test/TestCliente.java
+++ TrabajoFinalTaller/src/main/java/test/TestCliente.java
@@ -9,20 +9,26 @@ import modeloDatos.EmpleadoPretenso;
 import modeloDatos.Empleador;
 import util.Constantes;
 
-public class TestModeloNegocio {
+public class TestCliente {
 	
 	Empleador empleador;
 	EmpleadoPretenso empleado;
-	String testuser="pepe12";
-	String testpass="1234";
-	String testrealname="pepe";
-	String testtel="123456";
+	String testuserEmpleador="pepe12";
+	String testpassEmpleador="1234";
+	String testrealnameEmpleador="pepe";
+	String testtelEmpleador="123456";
+	String testuserEmpleado="pepe12";
+	String testpassEmpleado="1234";
+	String testrealnameEmpleado="pepe";
+	String testtelEmpleado="123456";
+	String testapellidoEmpleado="Lopez";
+	int testedadEmpleado=5;
 	
 	@Before
 	public void setUp() throws Exception {
-		empleador=new Empleador(testuser,testpass,testrealname,testtel,Constantes.SALUD,Constantes.JURIDICA);
+		empleador=new Empleador(testuserEmpleador,testpassEmpleador,testrealnameEmpleador,testtelEmpleador,Constantes.SALUD,Constantes.JURIDICA);
+		empleado=new EmpleadoPretenso(testuserEmpleado,testpassEmpleado,testrealnameEmpleado,testapellidoEmpleado,testtelEmpleado,testedadEmpleado);
 	}
-
 	@After
 	public void tearDown() throws Exception {
 	}
@@ -33,13 +39,13 @@ public class TestModeloNegocio {
 	@Test
 	public void testTel() {
 		String tel=empleador.getTelefono();
-		Assert.assertEquals("El telefono es incorrecto",testtel,tel);
+		Assert.assertEquals("El telefono es incorrecto",testtelEmpleador,tel);
 	}
 	
 	@Test
 	public void testUser() {
 		String username=empleador.getUsserName();
-		Assert.assertEquals("El username es incorrecto",testuser,username);
+		Assert.assertEquals("El username es incorrecto",testuserEmpleador,username);
 	}
 	@Test
 	public void testRubro() {
@@ -49,7 +55,7 @@ public class TestModeloNegocio {
 	@Test
 	public void testPass() {
 		String password=empleador.getPassword();
-		Assert.assertEquals("La password es incorrecto",testpass,password);
+		Assert.assertEquals("La password es incorrecto",testpassEmpleador,password);
 	}
 	@Test
 	public void testTipo() {
@@ -59,6 +65,6 @@ public class TestModeloNegocio {
 	@Test
 	public void testNombreReal() {
 		String nombrereal=empleador.getRealName();
-		Assert.assertEquals("El nombrereal es incorrecto",testrealname,nombrereal);
+		Assert.assertEquals("El nombrereal es incorrecto",testrealnameEmpleador,nombrereal);
 	}
 }
